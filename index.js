af357b820a0836980d2a355d9fcb6dc4e6a56082
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-04 05:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function takeANumber(katzDeliLine, name) {
 } 
 
 function nowServing(serving) {
-    var served = katzDeliLine.shift(); 
+    serving = katzDeliLine.shift(); 
     if (katzDeliLine.length === 0) {
         return 'There is nobody waiting to be served!';
     } else {
-       return 'Currently serving ' + served;
+       return 'Currently serving ' + serving;
     }
 }
 
